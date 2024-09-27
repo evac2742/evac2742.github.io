4060c4385d6a11c55ae3a4d22f58f6f4cf333eea
evac2742 <evac57@nycstudents.net>
2024-09-27 13:19:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = " Eva C "; // update the part BETWEEN the "quotes"
+let userName = " evac2742 "; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
